1d94a8895ced0cbd1fb77cd99d403894026ad3ec
Erfan Abdi <erfangplus@gmail.com>
2022-02-14 15:55:04 +0330

    Updater: Change lineage props to lmodroid
    Change-Id: I81546d752f4d5d9ad9ccf38d6c517fac406d44a8

--- push-update.sh
+++ push-update.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-updates_dir=/data/lineageos_updates
+updates_dir=/data/lmodroid_updates
 
 if [ ! -f "$1" ]; then
    echo "Usage: $0 ZIP [UNVERIFIED]"
    echo "Push ZIP to $updates_dir and add it to Updater"
    echo
-   echo "The name of ZIP is assumed to have lineage-VERSION-DATE-TYPE-* as format"
+   echo "The name of ZIP is assumed to have LMODroid-VERSION-DATE-TYPE-* as format"
    echo "If UNVERIFIED is set, the app will verify the update"
    exit
 fi
@@ -33,7 +33,7 @@ else
     status=2
 fi
 
-# Assume lineage-VERSION-DATE-TYPE-*.zip
+# Assume LMODroid-VERSION-DATE-TYPE-*.zip
 zip_name=`basename "$zip_path"`
 id=`echo "$zip_name" | sha1sum | cut -d' ' -f1`
 version=`echo "$zip_name" | cut -d'-' -f2`
