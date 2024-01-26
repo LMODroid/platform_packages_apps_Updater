LineageOS/android_packages_apps_Updater

Updater: Add prop for allowing major updates
Change-Id: Ida461cab7f0c4953fbdd6ea4928bf14fff386056

src/org/lineageos/updater/misc/Constants.java
@@ -46,6 +46,7 @@ public final class Constants {
     public static final String PROP_RELEASE_TYPE = "ro.lineage.releasetype";
     public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "lineage.updater.allow_downgrading";
     public static final String PROP_UPDATER_URI = "lineage.updater.uri";
+    public static final String PROP_UPDATER_ALLOW_MAJOR_UPDATE = "lineage.updater.allow_major_update";
 
     public static final String PREF_INSTALL_OLD_TIMESTAMP = "install_old_timestamp";
     public static final String PREF_INSTALL_NEW_TIMESTAMP = "install_new_timestamp";

LMODroid/platform_packages_apps_Updater

app/src/main/java/com/libremobileos/updater/misc/Constants.java
@@ -36,6 +36,7 @@ public final class Constants {
     public static final String PROP_RELEASE_TYPE = "ro.lmodroid.build_type";
     public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "lmodroid.updater.allow_downgrading";
     public static final String PROP_UPDATER_URI = "lmodroid.updater.uri";
+    public static final String PROP_UPDATER_ALLOW_MAJOR_UPDATE = "lmodroid.updater.allow_major_update";
 
     public static final String PREF_INSTALL_OLD_TIMESTAMP = "install_old_timestamp";
     public static final String PREF_INSTALL_NEW_TIMESTAMP = "install_new_timestamp";
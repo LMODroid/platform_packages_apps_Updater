43cb5ff8e0cff5632a3db9cec475f9ddb710faea
Erfan Abdi <erfangplus@gmail.com>
2022-04-26 00:18:03 +0430

    Updater: Add prop for allowing major updates
    Change-Id: Ida461cab7f0c4953fbdd6ea4928bf14fff386056

--- app/src/main/java/com/libremobileos/updater/misc/Constants.java
+++ app/src/main/java/com/libremobileos/updater/misc/Constants.java
@@ -36,6 +36,7 @@ public final class Constants {
     public static final String PROP_RELEASE_TYPE = "ro.lmodroid.build_type";
     public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "lmodroid.updater.allow_downgrading";
     public static final String PROP_UPDATER_URI = "lmodroid.updater.uri";
+    public static final String PROP_UPDATER_ALLOW_MAJOR_UPDATE = "lmodroid.updater.allow_major_update";
 
     public static final String PREF_INSTALL_OLD_TIMESTAMP = "install_old_timestamp";
     public static final String PREF_INSTALL_NEW_TIMESTAMP = "install_new_timestamp";
